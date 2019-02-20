bc4ec207ca5a20b9ff6cd148dff90b4b58601ae2
Fisser <mfisser@sva.de>
2019-02-20 10:08:12 +0100

    make it easy

--- datanode/run.sh
+++ datanode/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-datadir=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'`
+datadir=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'` # entfernen von file:// aus dem string
 if [ ! -d $datadir ]; then
   echo "Datanode data directory not found: $datadir"
   exit 2
 fi
 
-$HADOOP_PREFIX/bin/hdfs --config $HADOOP_CONF_DIR datanode
+$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
